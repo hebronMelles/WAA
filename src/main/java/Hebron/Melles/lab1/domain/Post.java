976d18639440eede74cdec843b4b4f9af40e010d
hebronMelles <147551219+hebronMelles@users.noreply.github.com>
2024-01-18 19:57:31 -0600

    lab2

--- src/main/java/Hebron/Melles/lab1/domain/Post.java
+++ src/main/java/Hebron/Melles/lab1/domain/Post.java
@@ -1,15 +1,22 @@
 package Hebron.Melles.lab1.domain;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
     String title;
     String content;
--- src/main/java/Hebron/Melles/lab1/repo/PostRepoImpl.java
+++ src/main/java/Hebron/Melles/lab1/repo/PostRepoImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
+
 public class PostRepoImpl implements PostRepo{
     private static List<Post> posts;
 
